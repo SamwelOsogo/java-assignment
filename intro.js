e5dbc2b9eeaa345af46522a0abc08c5d72f45818
SamwelOsogo <samwelosogo5@gmail.com>
2026-05-15 16:20:27 +0300

    add intro.js
    add intro.js

---
+++ intro.js
@@ -0,0 +1,9 @@
+
+const myName = "samwel osogo";
+const favoriteLanguage = "JavaScript";
+const myAge = 25;
+
+console.log("My name is " + myName);
+console.log("My favorite programming language is " + favoriteLanguage);
+console.log("I am " + myAge + " years old");
+
